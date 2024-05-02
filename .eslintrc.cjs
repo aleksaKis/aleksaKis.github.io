0b5e6b60b7fbc20b844c3b476ebb310d0501987a
Aleksa Kis <kishaleksa@gmail.com>
2024-05-02 22:10:52 +0200

    feat: Enable tests

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,13 +4,25 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
         'plugin:react-hooks/recommended',
-        'prettier',
+        'plugin:jest-dom/recommended',
+        'plugin:prettier/recommended',
+        'plugin:testing-library/react',
     ],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    ignorePatterns: [
+        'dist',
+        '.eslintrc.cjs',
+        'config',
+        '*.config.*',
+        'node_modules',
+    ],
+
     parser: '@typescript-eslint/parser',
     plugins: ['react-refresh'],
     rules: {
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-uses-react': 'off',
         'react-refresh/only-export-components': [
             'warn',
             { allowConstantExport: true },
