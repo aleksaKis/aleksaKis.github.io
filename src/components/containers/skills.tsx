82d2cd1201814caa8e43b3f8454dcf42e3de361f
Aleksa Kis <kishaleksa@gmail.com>
2024-06-05 23:08:20 +0200

    wip

--- src/components/containers/skills.tsx
+++ src/components/containers/skills.tsx
@@ -12,16 +12,24 @@ const words = [
 
 export const Skills = () => {
     return (
-        <div className="min-h-svh flex justify-center items-center">
-            <h4 className="text-3xl overflow-hidden text-neutral-100 z-50">
+        <motion.div
+            className="min-h-svh flex justify-center items-center relative"
+            initial={{ opacity: 0 }}
+            transition={{ duration: 1.5, delay: 0.5, ease: 'easeInOut' }}
+            whileInView={{ opacity: 1 }}
+        >
+            <h4 className="sm:text-3xl text-2xl overflow-hidden text-neutral-100 z-50">
                 I create
                 <FlipWords words={words} />
                 web applications.{' '}
                 <motion.button
+                    style={{ position: 'absolute' }}
+                    transition={{ duration: 0.8, ease: 'easeIn' }}
                     whileTap={{
-                        scale: 0.9,
-                        rotate: 360,
-                        transition: { duration: 1 },
+                        scale: 0,
+                        rotate: 30,
+                        top: '200px',
+                        right: '30px',
                     }}
                 >
                     🚀
@@ -29,6 +37,6 @@ export const Skills = () => {
                 <br />
                 Making the web a better place, one line of code at a time.
             </h4>
-        </div>
+        </motion.div>
     );
 };
