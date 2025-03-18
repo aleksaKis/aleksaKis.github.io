b0e8e76337c6afe58717d48f168a4741ed1529f8
Aleksa Kis <kishaleksa@gmail.com>
2025-03-18 21:47:15 +0100

    feat(analytics): Add PostHog analytics configuration and setup

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_PUBLIC_POSTHOG_KEY: string;
+    readonly VITE_PUBLIC_POSTHOG_HOST: string;
+    // more env variables...
+}
+
+interface ImportMeta {
+    readonly VITE_PUBLIC_POSTHOG_KEY: ImportMetaEnv;
+    readonly VITE_PUBLIC_POSTHOG_HOST: ImportMetaEnv;
+}
