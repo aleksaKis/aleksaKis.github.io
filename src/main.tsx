44d2b086f0bd88bdd9d5c98531c106aab86ec2dd
Aleksa Kis <59057989+aleksaKis@users.noreply.github.com>
2025-03-18 21:49:00 +0100

    feat(analytics): Add PostHog analytics configuration and setup (#7)
    * chore(deps): bump rollup from 4.21.0 to 4.24.0
    
    Bumps [rollup](https://github.com/rollup/rollup) from 4.21.0 to 4.24.0.
    - [Release notes](https://github.com/rollup/rollup/releases)
    - [Changelog](https://github.com/rollup/rollup/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/rollup/rollup/compare/v4.21.0...v4.24.0)
    
    ---
    updated-dependencies:
    - dependency-name: rollup
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * update package.json
    
    * feat(analytics): Add PostHog analytics configuration and setup
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
+import { PostHogProvider } from 'posthog-js/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
 import './index.css';
 
+const options = {
+    api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+};
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
+        <PostHogProvider
+            apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+            options={options}
+        >
+            <App />
+        </PostHogProvider>
     </React.StrictMode>,
 );
